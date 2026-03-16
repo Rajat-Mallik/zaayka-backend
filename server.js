c9961712de2cc69ada1c608977737eb18ba3c1f9
Rajat Malik <malik.rajat19102001@gmail.com>
2026-03-16 23:31:25 +0530

    added app.use(express.urlencoded({ extended: true }));

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: function (origin, callback) {
     if (
